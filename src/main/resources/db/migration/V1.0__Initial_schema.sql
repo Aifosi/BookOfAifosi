91d4e00a3d2e397ff86cd3bc52b1693fd6e18a56
Aifosi <107549080+Aifosi@users.noreply.github.com>
2022-06-23 21:31:23 +0100

    Remove unused tables from schema

--- src/main/resources/db/migration/V1.0__Initial_schema.sql
+++ src/main/resources/db/migration/V1.0__Initial_schema.sql
@@ -1,27 +1,9 @@
-CREATE TABLE tasks (
-  id          uuid PRIMARY KEY                  DEFAULT gen_random_uuid(),
-  name        text                     NOT NULL,
-  description text                     NOT NULL,
-  created_at  timestamp WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  updated_at  timestamp WITH TIME ZONE NOT NULL DEFAULT NOW()
-);
-
 CREATE TABLE tags (
   id          uuid PRIMARY KEY                  DEFAULT gen_random_uuid(),
   name        text                     NOT NULL,
   description text                     NULL     DEFAULT NULL,
   created_at  timestamp WITH TIME ZONE NOT NULL DEFAULT NOW(),
   updated_at  timestamp WITH TIME ZONE NOT NULL DEFAULT NOW()
-);
-
-CREATE TABLE task_tags (
-  id           uuid PRIMARY KEY                  DEFAULT gen_random_uuid(),
-  task         uuid                     NOT NULL REFERENCES tasks (id),
-  tag          uuid                     NOT NULL REFERENCES tags (id),
-  is_blacklist boolean                  NOT NULL,
-  created_at   timestamp WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  updated_at   timestamp WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  UNIQUE (task, tag)
 );
 
 CREATE TABLE users (
