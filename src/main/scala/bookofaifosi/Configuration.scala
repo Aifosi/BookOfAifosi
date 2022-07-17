f95fab9b8b7a1f9a4e29266d657d0b974048bf9e
Aifosi <107549080+Aifosi@users.noreply.github.com>
2022-07-17 20:42:20 +0100

    Log to channel (#2)
    Log commands to channel

--- src/main/scala/bookofaifosi/Configuration.scala
+++ src/main/scala/bookofaifosi/Configuration.scala
@@ -1,8 +1,11 @@
 package bookofaifosi
 
+import bookofaifosi.model.{Channel, DiscordID}
 import com.typesafe.config.{Config, ConfigFactory}
 import pureconfig.{ConfigReader, ConfigSource}
 import pureconfig.generic.derivation.default.derived
+import cats.syntax.traverse.*
+import cats.effect.IO
 
 import scala.concurrent.duration.FiniteDuration
 
@@ -18,9 +21,10 @@ case class Configuration(
   publicPort: Int,
   checkFrequency: FiniteDuration,
   pilloryBitches: PilloryBitches,
-) derives ConfigReader
+  logChannelId: Option[DiscordID],
+) derives ConfigReader:
+  lazy val logChannel: IO[Option[Channel]] = Bot.config.logChannelId.traverse(logChannelId => Bot.discord.get.flatMap(_.channelByID(logChannelId)))
 
 object Configuration:
-
   def fromConfig(config: Config = ConfigFactory.load()): Configuration =
     ConfigSource.fromConfig(config).at("app").loadOrThrow[Configuration]
